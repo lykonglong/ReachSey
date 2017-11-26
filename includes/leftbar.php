4e1810504c2f2943bf4830e6436c8885e2a70367
lykonglong@qq.com <lykonglong@qq.com>
2017-11-26 21:54:54 +0700

    fix leftbar

--- includes/leftbar.php
+++ includes/leftbar.php
@@ -2,21 +2,27 @@
     <div class="list-group">
 
         <div class="leftbar">
-            <h2 class="title">置顶推荐</h2>
-            <ul>
-                <li class="item"><a href="10/803953.html"><img src="img/King Arthur.jpg" alt="SolidWorks 2018 SP0.1 Full Premium Multilanguage x64" /><p>SolidWorks 2018 SP0.1 Full Premium Multilanguage x64</p><p class="muted">
-                            SOLIDWORKS® 2018 提供了许多增强和改进功能，其中大多数是直接针对客户要求而做出...</p></a></li>
 
-                <li class="item"><a href="10/801054.html"><img src="img/King Arthur.jpg" alt="MathWorks MATLAB R2017b x64" /><p>MathWorks MATLAB R2017b x64</p><p class="muted">
-                            Matlab R2017b win x64 版本正式发布！
-                            MATLAB 是美国MathWor...</p></a></li>
+              <h2 class="title">Poppulars</h2>
+              <ul>
+                <li class="item"><a href="10/803953.html"><img src="img/King Arthur.jpg" alt="SolidWorks 2018 SP0.1 Full Premium Multilanguage x64" /><h3>SolidWorks 2018 SP0.1 Full Premium Multilanguage x64</h3></a></li>
+                <li class="item"><a href="10/803953.html"><img src="img/King Arthur.jpg" alt="SolidWorks 2018 SP0.1 Full Premium Multilanguage x64" /><h3>SolidWorks 2018 SP0.1 Full Premium Multilanguage x64</h3></a></li>
+                <li class="item"><a href="10/803953.html"><img src="img/King Arthur.jpg" alt="SolidWorks 2018 SP0.1 Full Premium Multilanguage x64" /><h3>SolidWorks 2018 SP0.1 Full Premium Multilanguage x64</h3></a></li>
+                <li class="item"><a href="10/803953.html"><img src="img/King Arthur.jpg" alt="SolidWorks 2018 SP0.1 Full Premium Multilanguage x64" /><h3>SolidWorks 2018 SP0.1 Full Premium Multilanguage x64</h3></a></li>
 
-                <li class="item"><a href="09/674513.html"><img src="img/King Arthur.jpg" alt="PTC Creo 4.0 M030" /><p>PTC Creo 4.0 M030</p><p class="muted">PTC公司正式发布了Creo 4.0 正式版 版本号 M030（2017-9月更新） ，其革命性...</p></a></li>
+              </ul>
 
-                <li class="item"><a href="09/655197.html"><img src="img/King Arthur.jpg" alt="CSI Bridge 2017 v19.0.0 x32/x64" /><p>CSI Bridge 2017 v19.0.0 x32/x64</p><p class="muted">
-                            CSI Bridge 2017新版发布！
-                            CSi Bridge集成了桥梁结构的建模、分析和设...</p></a></li>
-            </ul>
+              <h2 class="title">Tags</h2>
+              <div class="d_tags">
+                  <a  href="tags8f8b.html?/%D7%DB%BA%CF%BD%CC%B3%CC/"> 3D Animation (127) </a>
+                  <a  href="tags8f8b.html?/%D7%DB%BA%CF%BD%CC%B3%CC/"> Photoshop (44) </a>
+                  <a  href="tags8f8b.html?/%D7%DB%BA%CF%BD%CC%B3%CC/"> Windows (13) </a>
+                  <a  href="tags8f8b.html?/%D7%DB%BA%CF%BD%CC%B3%CC/"> Udemy (20) </a>
+                  <a  href="tags8f8b.html?/%D7%DB%BA%CF%BD%CC%B3%CC/"> Lynda (50) </a>
+                  <a  href="tags8f8b.html?/%D7%DB%BA%CF%BD%CC%B3%CC/"> Plural Sight (200) </a>
+                  <a  href="tags8f8b.html?/%D7%DB%BA%CF%BD%CC%B3%CC/"> Tutorials (1500) </a>
+                  <a  href="tags8f8b.html?/%D7%DB%BA%CF%BD%CC%B3%CC/"> Design (200) </a>
+              </div>
         </div>
 
 
--- index.php
+++ index.php
@@ -12,17 +12,14 @@
           <div class="sticky">
               <h2 class="title">置顶推荐</h2>
               <ul>
-                <li class="item"><a href="10/803953.html"><img src="img/King Arthur.jpg" alt="SolidWorks 2018 SP0.1 Full Premium Multilanguage x64" /><h3>SolidWorks 2018 SP0.1 Full Premium Multilanguage x64</h3><p class="muted">
-                  SOLIDWORKS® 2018 提供了许多增强和改进功能，其中大多数是直接针对客户要求而做出...</p></a></li>
+                <li class="item"><a href="10/803953.html"><img src="img/King Arthur.jpg" alt="SolidWorks 2018 SP0.1 Full Premium Multilanguage x64" /><h3>SolidWorks 2018 SP0.1 Full Premium Multilanguage x64</h3><p class="muted">SOLIDWORKS® 2018 提供了许多增强和改进功能，其中大多数是直接针对客户要求而做出...</p></a></li>
 
-                <li class="item"><a href="10/801054.html"><img src="img/King Arthur.jpg" alt="MathWorks MATLAB R2017b x64" /><h3>MathWorks MATLAB R2017b x64</h3><p class="muted">
-                  Matlab R2017b win x64 版本正式发布！
+                <li class="item"><a href="10/801054.html"><img src="img/King Arthur.jpg" alt="MathWorks MATLAB R2017b x64" /><h3>MathWorks MATLAB R2017b x64</h3><p class="muted">Matlab R2017b win x64 版本正式发布！
                   MATLAB 是美国MathWor...</p></a></li>
 
                 <li class="item"><a href="09/674513.html"><img src="img/King Arthur.jpg" alt="PTC Creo 4.0 M030" /><h3>PTC Creo 4.0 M030</h3><p class="muted">PTC公司正式发布了Creo 4.0 正式版 版本号 M030（2017-9月更新） ，其革命性...</p></a></li>
 
-                <li class="item"><a href="09/655197.html"><img src="img/King Arthur.jpg" alt="CSI Bridge 2017 v19.0.0 x32/x64" /><h3>CSI Bridge 2017 v19.0.0 x32/x64</h3><p class="muted">
-                  CSI Bridge 2017新版发布！
+                <li class="item"><a href="09/655197.html"><img src="img/King Arthur.jpg" alt="CSI Bridge 2017 v19.0.0 x32/x64" /><h3>CSI Bridge 2017 v19.0.0 x32/x64</h3><p class="muted">CSI Bridge 2017新版发布！
                   CSi Bridge集成了桥梁结构的建模、分析和设...</p></a></li>
               </ul>
             </div>
